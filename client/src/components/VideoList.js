bc3669abf9ba3501d4380d4eeba555dbf5e52f63
Michael Trevino <mtrevin93@gmail.com>
2021-10-07 11:53:07 -0400

    added client react app

---
+++ client/src/components/VideoList.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { getAllVideos } from "../modules/videoManager";
+
+const VideoList = () => {
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = () => {
+    getAllVideos().then(videos => setVideos(videos));
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div>
+      {videos.map(video => 
+        <Video video = {video} key = {video.id} />
+      )}
+    </div>
+  );
+}
+
+export default VideoList;
